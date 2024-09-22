24109b874b637edb878e85936d33484e1579bc3e
Md. Yasin Ahmed Mahi <121636177+mdyasinahmed@users.noreply.github.com>
2024-09-22 11:41:36 +0600

    Add lab 2 task_1.asm

---
+++ lab_task/lab_2/task_1.asm
@@ -0,0 +1,43 @@
+.MODEL SMALL
+.STACK 100H
+.DATA
+    X DW 6      ; You can change these values as needed
+    Y DW 3
+    Z DW 2
+    RESULT1 DW ?
+    RESULT2 DW ?
+    RESULT3 DW ?
+    
+.CODE
+MAIN PROC
+    ; Initialize DS
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    ; 1. X * Y
+    MOV AX, X          ; Move X into AX
+    MOV BX, Y          ; Move Y into BX
+    IMUL BX            ; AX = AX * BX
+    MOV RESULT1, AX    ; Store the result in RESULT1
+    
+    ; 2. X / Y
+    MOV AX, X          ; Move X into AX
+    MOV BX, Y          ; Move BX with Y
+    XOR DX, DX         ; Clear DX before division
+    IDIV BX            ; AX = AX / BX, remainder in DX
+    MOV RESULT2, AX    ; Store the result in RESULT2
+    
+    ; 3. X * Y / Z
+    MOV AX, X          ; Move X into AX
+    MOV BX, Y          ; Move Y into BX
+    IMUL BX            ; AX = AX * BX
+    MOV BX, Z          ; Move Z into BX
+    IDIV BX            ; AX = AX / BX
+    MOV RESULT3, AX    ; Store the result in RESULT3
+    
+    ; Exit to DOS
+    MOV AX, 4C00H
+    INT 21H
+
+MAIN ENDP
+END MAIN
