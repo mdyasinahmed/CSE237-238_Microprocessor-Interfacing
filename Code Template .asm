c11915b762736c067f8357bcc2b2ffa44b901dfe
mdyasinahmed <yasinahmed7875@gmail.com>
2024-11-22 20:50:08 +0600

    add code template of assembly

---
+++ Code Template .asm
@@ -0,0 +1,30 @@
+.MODEL SMALL 
+  
+.STACK 100H 
+ 
+.DATA  
+ 
+ 
+.CODE  
+MAIN PROC  
+ 
+;iniitialize DS 
+ 
+MOV AX,@DATA  
+MOV DS,AX  
+  
+;enter your code here 
+ 
+ 
+ 
+ 
+ 
+  
+ 
+;exit to DOS  
+                
+MOV AX,4C00H 
+INT 21H  
+ 
+MAIN ENDP 
+END MAIN  
