695e2918bd13ff0b8d91ffce35c61c174d55c423
mdyasinahmed <yasinahmed7875@gmail.com>
2024-11-24 09:59:20 +0600

    add lab 5 task 2 (a) practice code

---
+++ Lab_5/L5_task_02_a.asm
@@ -0,0 +1,33 @@
+.MODEL SMALL 
+  
+.STACK 100H 
+ 
+.DATA  
+.CODE  
+MAIN PROC  
+ 
+;iniitialize DS 
+ 
+MOV AX,@DATA  
+MOV DS,AX  
+  
+;enter your code here 
+    MOV BX, 1
+    MOV AX, 0
+    
+    start:
+    CMP BX, 148
+    JG end
+    
+    ADD AX, BX
+    ADD BX, 3
+    
+    JMP start
+
+end:    
+;exit to DOS                  
+MOV AX,4C00H 
+INT 21H  
+ 
+MAIN ENDP 
+END MAIN  
